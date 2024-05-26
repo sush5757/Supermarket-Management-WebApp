0339e724496aa21d09b7120732485100ca7ed83e
sushant manjare <sushantmanjare17269@gmail.com>
2024-05-26 20:41:00 +0530

    product

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+using WebApp.ViewModels;
+
+namespace WebApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = ProductsRepository.GetProducts(loadCategory : true);
+            return View(products);
+        }
+
+        public IActionResult Add()
+        {
+
+            var productViewModel = new ProductViewModel
+            {
+                Categories = CategoriesRepository.GetCategories()
+            };
+            return View(productViewModel);
+        }
+        [HttpPost]
+        public IActionResult Add(ProductViewModel productViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                ProductsRepository.AddProduct(productViewModel.Product);
+                return RedirectToAction(nameof(Index));
+            }
+            productViewModel.Categories = CategoriesRepository.GetCategories();
+            return View(productViewModel);
+        }
+        public IActionResult Edit(int id)
+        {
+            var productViewModel = new ProductViewModel
+            {
+                Product = ProductsRepository.GetProductById(id) ?? new Product(),
+                Categories = CategoriesRepository.GetCategories()
+            };
+            return View(productViewModel);
+           
+
+        }
+        [HttpPost]
+        public IActionResult Edit(ProductViewModel productViewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                ProductsRepository.UpdateProduct(productViewModel.Product.ProductId, productViewModel.Product);
+                return RedirectToAction(nameof(Index));
+            }
+
+            productViewModel.Categories = CategoriesRepository.GetCategories();
+            return View(productViewModel);
+        }
+    }
+}
